fa8a5ffe2f4f70f95c824f3b0d5fefeb2186a038
Rainer Stropek <rainer@timecockpit.com>
2024-10-04 12:41:18 +0200

    Saved in TypeScript Playground at 2024-10-04T10:41:17.783Z

---
+++ color_areas/index.ts
@@ -0,0 +1,32 @@
+function setup() {
+    createCanvas(400, 200);
+    background("black");
+    noStroke();
+
+    fill("yellow")
+    rect(0, 0, width / 3);
+    fill("red")
+    rect(width  / 3, 0, width / 3);
+    fill("green")
+    rect(width * 2 / 3, 0, width / 3);
+}
+
+
+function mouseClicked() {
+    textSize(30);
+    fill("white");
+    textAlign(CENTER);
+    if (mouseX < width / 3) {
+        text("Yellow", width / 2, height - 30)
+    }
+
+    if (mouseX > width / 3 || mouseX < width * 2 / 3) {
+        text("Red", width / 2, height - 30)
+    } 
+
+    else {
+        text("Green", width / 2, height - 30)
+    }
+
+}
+
