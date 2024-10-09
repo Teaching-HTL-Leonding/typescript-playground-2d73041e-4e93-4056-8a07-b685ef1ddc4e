9898cb7499a36c7ca1a4720e34471620e2e43dee
Rainer Stropek <rainer@timecockpit.com>
2024-10-09 14:12:04 +0200

    Saved in TypeScript Playground at 2024-10-09T12:12:03.899Z

--- color_quadrants/index.ts
+++ color_quadrants/index.ts
@@ -3,16 +3,15 @@ function setup() {
     background("black");
     noStroke();
 
-
     // Color Quadrants
     fill("yellow");
-    rect(0, 0, width /2);
+    rect(0, 0, width / 2);
     fill("blue");
-    rect(0, height /2, width /2);
+    rect(0, height / 2, width / 2);
     fill("green");
-    rect(width / 2, 0, width /2);
+    rect(width / 2, 0, width / 2);
     fill("red");
-    rect(width / 2, height / 2, width /2);
+    rect(width / 2, height / 2, width / 2);
 
     // Selected Color Display
     fill("black");
@@ -23,27 +22,27 @@ function setup() {
 let selectedColor;
 
 function mouseClicked() {
-    // Selected Color Display
+    // Check which color is selected
+    if (mouseX < width / 2 && mouseY < height / 2) {
+        selectedColor = "Yellow";
+    }
+    if (mouseX > width / 2 && mouseY < height / 2) {
+        selectedColor = "Green";
+    }
+    if (mouseX < width / 2 && mouseY > height / 2) {
+        selectedColor = "Blue";
+    }
+    if (mouseX > width / 2 && mouseY > height / 2) {
+        selectedColor = "Red";
+    }
+
+    // Clear and update the Selected Color Display
     fill("black");
     rect(width / 2 * 0.5, height / 2 * 0.5, width / 4);
     rect(width / 2 * 1, height / 2 * 0.5, width / 4);
-
 
     textAlign(CENTER);
     fill("white");
     textSize(30);
     text(selectedColor, width / 2, height / 2 * 1.1);
-
-    if(mouseX < width / 2 && mouseY < height / 2) {
-        selectedColor = "Yellow";
-    }
-    if(mouseX > width / 2 && mouseY < height / 2) {
-        selectedColor = "Green";
-    }
-    if(mouseX < width / 2 && mouseY > height / 2) {
-        selectedColor = "Blue";
-    }
-    if(mouseX > width / 2 && mouseY > height / 2) {
-        selectedColor = "Red";
-    }
 }
