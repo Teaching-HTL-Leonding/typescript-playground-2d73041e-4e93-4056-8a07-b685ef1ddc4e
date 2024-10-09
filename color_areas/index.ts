0e609c6176a8b10a2d47e3287f32ed8aa036da5b
Rainer Stropek <rainer@timecockpit.com>
2024-10-09 14:09:46 +0200

    Saved in TypeScript Playground at 2024-10-09T12:09:45.866Z

--- color_areas/index.ts
+++ color_areas/index.ts
@@ -3,39 +3,37 @@ function setup() {
     background("black");
     noStroke();
 
-    fill("yellow")
+    fill("yellow");
     rect(0, 0, width / 3);
-    fill("red")
-    rect(width  / 3, 0, width / 3);
-    fill("green")
+    fill("red");
+    rect(width / 3, 0, width / 3);
+    fill("green");
     rect(width * 2 / 3, 0, width / 3);
 }
 
-
 function mouseClicked() {
-
-    fill("black")
-    rect(0, height / 6.6, width)
+    fill("black");
+    rect(0, height / 6.6, width);
     textSize(30);
     fill("white");
     textAlign(CENTER);
+
     if (mouseX < width / 3) {
-        text("Yellow", width / 2, height - 30)
+        text("Yellow", width / 2, height - 30);
     }
 
-    if (mouseX > width / 3 || width < width / 3 * 2) {
-        text("Red", width / 2, height - 30)
-    } 
-
-    if (mouseX > width / 3 * 2) {
-        text("Green", width / 2, height - 30)
+    if (mouseX > width / 3 && mouseX < width * 2 / 3) {
+        text("Red", width / 2, height - 30);
     }
 
-    fill("yellow")
+    if (mouseX > width * 2 / 3) {
+        text("Green", width / 2, height - 30);
+    }
+
+    fill("yellow");
     rect(0, 0, width / 3);
-    fill("red")
-    rect(width  / 3, 0, width / 3);
-    fill("green")
+    fill("red");
+    rect(width / 3, 0, width / 3);
+    fill("green");
     rect(width * 2 / 3, 0, width / 3);
-
 }
