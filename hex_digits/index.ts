fd102cf656cb325a2e6b0feb1e9c0a6f6223d1a2
Rainer Stropek <rainer@timecockpit.com>
2024-10-14 19:26:43 +0200

    Saved in TypeScript Playground at 2024-10-14T17:26:42.733Z

--- hex_digits/index.ts
+++ hex_digits/index.ts
@@ -1,31 +1,65 @@
 function setup() {
   createCanvas(500, 500);
   background("black");
-  const num = Math.floor(random(0, 10000));
+  const num = Math.floor(random(0, 4095));
+
   // Add the necessary code here
 
   stroke("yellow");
   noFill();
-  rect(width / 3, height / 3, width / 6, height / 4);
-  rect(width / 2, height / 3, width / 6, height / 4);
-  rect(width / 6, height / 3, width / 6, height / 4);
+  rect(width / 10.5 * 2.5, height / 3, width / 10, height / 4);
+  rect(width / 10.5 * 4, height / 3, width / 10, height / 4);
+  rect(width / 10.5 * 5.5, height / 3, width / 10, height / 4);
+
+  let newNum1 = Math.floor(num / 16);
+  let newNum2 = Math.floor(newNum1 / 16);
+  let newNum3 = Math.floor(newNum2 / 16);
 
 
-  let newNum1 = Math.floor(num / 16);  // Erste Division, gibt den Quotienten
-  let remainder1 = num % 16;  // Erster Rest (letzte Hexadezimalstelle)
-
-  let newNum2 = Math.floor(newNum1 / 16);  // Zweite Division
-  let remainder2 = newNum1 % 16;  // Zweiter Rest
-
-  let newNum3 = Math.floor(newNum2 / 16);  // Dritte Division (Endwert wird 0)
-  let remainder3 = newNum2 % 16;  // Dritter Rest
-
+  // Berechungn
   textAlign(CENTER, CENTER);
-  text(`${Math.floor(remainder3)}`, width / 6 * 1.2, height / 3, width / 10, height / 4);
-  text(`${Math.floor(remainder2)}`, width / 3 * 1.1, height / 3, width / 10, height / 4);
-  text(`${Math.floor(remainder1)}`, width / 3 * 1.6, height / 3, width / 10, height / 4);
 
-  
+  text(`${Math.floor((newNum1 % 16))}`, width / 10.5 * 2.5, height / 3, width / 10, height / 4);
+  text(`${Math.floor((newNum2 % 16))}`, width / 10.5 * 4, height / 3, width / 10, height / 4);
+  text(`${Math.floor((newNum3 % 16))}`, width / 10.5 * 5.5, height / 3, width / 10, height / 4);
+
+
+  if (num % 16 === 10) {
+    fill("black");
+    rect(width / 10.5 * 2.5, height / 3, width / 10, height / 4);
+    text(`A`, width / 10.5 * 2.5, height / 3, width / 10, height / 4);
+  }
+
+  if (num % 16 === 11) {
+  fill("black");
+  rect(width / 10.5 * 2.5, height / 3, width / 10, height / 4);
+  text(`B`, width / 10.5 * 2.5, height / 3, width / 10, height / 4);
+}
+
+  if (num % 16 === 12) {
+  fill("black");
+  rect(width / 10.5 * 2.5, height / 3, width / 10, height / 4);
+  text(`C`, width / 10.5 * 2.5, height / 3, width / 10, height / 4);
+}
+
+  if (num % 16 === 13) {
+  fill("black");
+  rect(width / 10.5 * 2.5, height / 3, width / 10, height / 4);
+  text(`D`, width / 10.5 * 2.5, height / 3, width / 10, height / 4);
+}
+
+  if (num % 16 === 14) {
+  fill("black");
+  rect(width / 10.5 * 2.5, height / 3, width / 10, height / 4);
+  text(`E`, width / 10.5 * 2.5, height / 3, width / 10, height / 4);
+}
+
+  if (num % 16 === 15) {
+  fill("black");
+  rect(width / 10.5 * 2.5, height / 3, width / 10, height / 4);
+  text(`F`, width / 10.5 * 2.5, height / 3, width / 10, height / 4);
+}
+
 
   textAlign(CENTER, CENTER);
   fill("yellow");
