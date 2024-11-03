54359734844877ee06e69e411c89a2ae80189ebc
Rainer Stropek <rainer@timecockpit.com>
2024-11-03 17:40:43 +0100

    Saved in TypeScript Playground at 2024-11-03T16:40:42.743Z

--- calculator_if/index.ts
+++ calculator_if/index.ts
@@ -5,7 +5,7 @@ let lineHeight: number = 0;   // Height of a line of the responsive layout
 let cellWidth: number = 0;    // Width of a cell of the responsive layout
 
 function setup() {
-    createCanvas(300, 600);
+    createCanvas(500, 400);
     lineHeight = height / 5;
     cellWidth = width / 3;
 }
@@ -44,5 +44,8 @@ function draw() {
 }
 
 function mouseClicked() {
-    // <<< Add code here
+    if(mouseX > 0 && mouseX < height / 5) {
+        num = 7;
+        console.log(num);
+    }
 }
