150683e07b93ca4d2af544c563591a259a49b6bf
Rainer Stropek <rainer@timecockpit.com>
2024-11-06 15:06:23 +0100

    Saved in TypeScript Playground at 2024-11-06T14:06:23.198Z

--- binary_guesser/index.ts
+++ binary_guesser/index.ts
@@ -32,19 +32,48 @@ let digi4 = 0;
 
 function mouseClicked() {
   if (mouseX > width / 6 && mouseX < width / 6 * 2 - 20 && mouseY > height / 3 && mouseY < height / 3 * 2) {
-    digi1 = 1;
-    fill("black");
-    noStroke;
-    rect(width / 6, height / 3, width / 8, height / 3)
-    fill("yellow");
-    text(`${digi1}`, width / 4.4, height / 3 * 1.5)
+    if (digi1 === 0) {
+      digi1 = 1;
+      fill("black");
+      rect(width / 6, height / 3, width / 8, height / 3)
+      fill("yellow");
+      text(`${digi1}`, width / 4.4, height / 3 * 1.5)
+    } else if (digi1 === 1) {
+      digi1 = 0;
+      fill("black");
+      rect(width / 6, height / 3, width / 8, height / 3)
+      fill("yellow");
+      text(`${digi1}`, width / 4.4, height / 3 * 1.5)
+    }
   }
   if (mouseX > width / 6 * 2 && mouseX < width / 6 * 3 - 20 && mouseY > height / 3 && mouseY < height / 3 * 2) {
-    digi2 = 1;
-    fill("black");
-    noStroke;
-    rect(width / 6, height / 3, width / 8, height / 3)
-    fill("yellow");
-    text(`${digi1}`, width / 4.4, height / 3 * 1.5)
+    if (digi2 === 0) {
+      digi2 = 1;
+      fill("black");
+      rect(width / 6 * 2, height / 3, width / 8, height / 3)
+      fill("yellow");
+      text(`${digi2}`, width / 4.4 * 1.75, height / 3 * 1.5)
+    } else if (digi2 === 1) {
+      digi2 = 0;
+      fill("black");
+      rect(width / 6 * 2, height / 3, width / 8, height / 3)
+      fill("yellow");
+      text(`${digi2}`, width / 4.4 * 1.75, height / 3 * 1.5)
+    }
+  }
+  if (mouseX > width / 6 * 3 && mouseX < width / 6 * 4 - 20 && mouseY > height / 3 && mouseY < height / 3 * 2) {
+    if (digi2 === 0) {
+      digi2 = 1;
+      fill("black");
+      rect(width / 6 * 3, height / 3, width / 8, height / 3)
+      fill("yellow");
+      text(`${digi3}`, width / 4.4 * 2.49, height / 3 * 1.5)
+    } else if (digi2 === 1) {
+      digi2 = 0;
+      fill("black");
+      rect(width / 6 * 3, height / 3, width / 8, height / 3)
+      fill("yellow");
+      text(`${digi3}`, width / 4.4 * 2.49, height / 3 * 1.5)
+    }
   }
 }
